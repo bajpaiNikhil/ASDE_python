122a4eef78e809fbf882121126c3e3b7e3b7b0b4
NikhilBajpai <nikhil.cse16@gmail.com>
2022-05-30 11:49:22 +0530

    day 3

---
+++ Value equal to index value.py
@@ -0,0 +1,7 @@
+arr = [15, 2, 45, 12, 7]
+
+for i,j in enumerate(arr):
+    print(i ,j)
+    if i+1 == j:
+        print(j)
+        break
--- rank the permutation.py
+++ rank the permutation.py
@@ -1,6 +1,6 @@
 from itertools import permutations
 
-s = "cba"
+s = "epwlz"
 
 
 # l = sorted(s)
@@ -23,26 +23,45 @@ def factorial(s):
     return s * factorial(s - 1)
 
 
+#
+# def findRank(s):
+#     l = sorted(s)
+#     ll = "".join(l)
+#     #print(l)
+#     checkList = [1] * (len(s))
+#     #print(checkList)
+#     ans = 0
+#     indx = 0
+#     for i in range(0, len(s)):
+#         if checkList[i] == -1:
+#             continue
+#         elif s[indx] != ll[i]:
+#             print(len(s) , indx , (len(s)-indx-1))
+#             ans += factorial(len(s) - indx - 1)
+#             print(ans)
+#         else:
+#             i = -1
+#             indx += 1
+#             checkList[i] = -1
+#     return ans + 1
+
 def findRank(s):
-    l = sorted(s)
-    ll = "".join(l)
-    #print(l)
-    checkList = [1] * (len(s))
-    #print(checkList)
+    smallerElements = []
     ans = 0
-    indx = 0
-    for i in range(0, len(s)):
-        if checkList[i] == -1:
-            continue
-        elif s[indx] != ll[i]:
-            print(len(s) , indx , (len(s)-indx-1))
-            ans += factorial(len(s) - indx - 1)
-            print(ans)
-        else:
-            i = -1
-            indx += 1
-            checkList[i] = -1
-    return ans + 1
+    for i in range(len(s)):
+        count = 0
+        for j in range(i + 1, len(s)):
+            if s[i] > s[j]:
+                count += 1
+        smallerElements.append(count)
+    print(smallerElements)
+
+    for i in range(len(s)):
+        ans+=smallerElements[i]*factorial(len(s)-1-i)
+    return ans+1
+
+
+
 
 
 print(findRank(s))
