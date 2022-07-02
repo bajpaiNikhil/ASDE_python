81e7537bb5424ea18428356740d558e8ba0288ef
NikhilBajpai <nikhil.cse16@gmail.com>
2022-07-02 19:21:36 +0530

    day 8 Sort

---
+++ Sort Array by Increasing Frequency.py
@@ -0,0 +1,16 @@
+
+from collections import defaultdict
+from  collections import  Counter
+nums = [-1,1,-6,4,5,-6,1,4,1]
+
+hashMap = Counter(nums).most_common()
+print(hashMap)
+hashMap.sort(key = lambda x: x[0], reverse=True)
+hashMap.sort(key = lambda x:x[1])
+print(hashMap)
+l = []
+for i in hashMap:
+    print(i)
+    a,b = i
+    l.extend([a]*b)
+print(l)
